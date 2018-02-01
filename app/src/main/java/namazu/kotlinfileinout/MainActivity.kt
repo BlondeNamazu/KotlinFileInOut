924b96e9d5e36f5582b3833f488acc3c8cb92dee
Namazu <namazutitech15@gmail.com>
2018-02-01 20:10:46 +0900

    rewrite saveFile and readFile to use try-catch

--- app/src/main/java/namazu/kotlinfileinout/MainActivity.kt
+++ app/src/main/java/namazu/kotlinfileinout/MainActivity.kt
@@ -3,16 +3,14 @@ package namazu.kotlinfileinout
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RawRes
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import java.io.BufferedReader
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.InputStreamReader
 import kotlinx.android.synthetic.main.activity_main.*;
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button_save.setOnClickListener({
-            var text : String = edit_text.getText().toString()
+            val text : String = edit_text.getText().toString()
             saveFile(fileName,text)
             if(text.length == 0){
                 text_view.setText(R.string.no_text)
@@ -44,23 +42,27 @@ class MainActivity : AppCompatActivity() {
 
     // save written string to "testfile.txt"
     public fun saveFile(file : String,str : String){
-        var fileOutputstream : FileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
-        fileOutputstream?.write(str.toByteArray())
+        try {
+            val out = openFileOutput(file,Context.MODE_PRIVATE)
+            out.write(str.toByteArray())
+            out.close()
+        } catch (e: IOException){
+            e.printStackTrace()
+        }
     }
 
     // read "testfile.txt" and show its content
     public fun readFile(file : String) : String? {
-        var text : String? = null;
-        val fileInputStream : FileInputStream? = openFileInput(file)
-        if(fileInputStream == null)Log.d("mydebug","fileInputStream is null")
-        val reader : BufferedReader = BufferedReader(InputStreamReader(fileInputStream,"UTF-8"))
-        if(reader == null)Log.d("mydebug","reader is null")
-        var lineBuffer : String? = null;
-        do {
-            lineBuffer = reader?.readLine()
-            if(lineBuffer != null)text = lineBuffer;
-        } while(lineBuffer != null)
-
+        var text : String = ""
+        try{
+            val reader = openFileInput(file).bufferedReader()
+            for(lineBuffer in reader.readLines()){
+                text += lineBuffer.toString()
+            }
+            reader.close()
+        } catch(e:FileNotFoundException){
+            e.printStackTrace()
+        }
         return text;
     }
 }
